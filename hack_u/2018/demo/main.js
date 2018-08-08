1da8cc2eb18e5d7a523b49352653356dbd52bada
Gorira <a5816083@aoyama.jp>
2018-08-08 17:18:41 +0900

    first

---
+++ hack_u/2018/demo/main.js
@@ -0,0 +1,32 @@
+function mouseover(t){
+	var id_v;
+	id_v=t.id;
+	console.log(id_v);
+	var color=$('#'+id_v).css('background-color');
+	if(color=="rgb(187, 187, 204)"){
+		$('#'+id_v).css('background-color','#aaccaa');
+	}else{
+		$('#'+id_v).css('background-color','#bbbbcc');
+	}
+}
+
+function mouseout(){
+	var i;
+	var flag=0;
+	for(i=1;i<=25;i++){
+		if($('#p'+i).css('background-color')==$('#a'+i).css('background-color')){
+			
+		}else{
+			flag=1;
+		}
+		
+		console.log(flag);
+		
+		if(flag==0){
+			TF.innerHTML="True";
+		}else{
+			TF.innerHTML="False";
+		}
+	}
+}
+
